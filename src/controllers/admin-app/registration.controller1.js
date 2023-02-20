40eae81a8084f4a6ebeeaf39fec086840f90399e
MuhammadnodirAnvarjonov <98937945+MuhammadnodirAnvarjonov@users.noreply.github.com>
2023-02-20 14:44:55 +0500

    tekshiruvs

--- src/controllers/admin-app/registration.controller1.js
+++ src/controllers/admin-app/registration.controller1.js
@@ -67,7 +67,9 @@ class RegistrationController {
                 } 
              ],
              limit: 200,
-             order: ['created_at', 'DESC']
+             order: [
+                ['created_at', 'DESC']
+             ]
         });
         res.status(200).send({  
             error: false,
@@ -120,7 +122,9 @@ class RegistrationController {
                 {model: Registration_payModel, as: 'registration_pay'}
             ],
             limit: 200,
-            order: ['created_at', 'DESC']
+            order: [
+                ['created_at', 'DESC']
+             ]
         });
         if (Prixod === null) {
             throw new HttpException(404, 'Not found');
