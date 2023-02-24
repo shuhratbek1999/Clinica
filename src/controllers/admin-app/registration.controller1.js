3fdc83b7d21eeb9c76f80a6e6f80567ba4d17e88
shuhratbek1999 <zokirjonovshuhratbek1119@gmail.com>
2023-02-24 22:20:50 +0500

    com

--- src/controllers/admin-app/registration.controller1.js
+++ src/controllers/admin-app/registration.controller1.js
@@ -1343,7 +1343,88 @@ class RegistrationController {
             message: 'Malumotlar o\'chirildi'
         });
     }
-     
+    delete = async (req, res, next) => {
+        const user = await ModelModel.findOne({
+            where:{
+                id: req.params.id
+            }
+          })
+          await QueueModel.destroy({
+            where:{
+                patient_id: user.dataValues.patient_id
+            }
+          })
+          await this.uplataModel.destroy({
+            where:{
+                user_id: user.dataValues.user_id
+            }
+          })
+      const doctor = await Registration_doctorModel.destroy({
+            where:{
+                registration_id: req.params.id
+            }
+           })
+           await Registration_filesModel.destroy({
+               where:{
+                registration_id: req.params.id
+               }
+              })
+            const inspection = await Registration_inspectionModel.destroy({
+               where:{
+                registration_id: req.params.id
+               }
+              })
+              await Registration_inspection_childModel.destroy({
+               where:{
+                registration_id: req.params.id
+               }
+              })
+              await Register_inspectionModel.destroy({
+                where:{
+                    doc_id: req.params.id
+                }
+              })
+              await RegisterDoctorModel.destroy({
+                where:{
+                    doc_id: req.params.id
+                }
+              })
+              await register_mkb.destroy({
+                where:{
+                    registration_id: req.params.id
+                }
+              })
+           const pay = await Registration_payModel.destroy({
+               where:{
+                registration_id: req.params.id
+               }
+              })
+              await Registration_recipeModel.destroy({
+               where:{
+                registration_id: req.params.id
+               }
+              })
+              await Register_kassaModel.destroy({
+                  where:{
+                      doctor_id: req.params.id,
+                      place: 'registration'
+                  }
+              })
+              const model =  await ModelModel.destroy({ 
+                where:{
+                  id: req.params.id
+                }
+            });
+        if(!model){
+            throw new HttpException(404, "bunday id yoq")
+        }
+        res.status(200).send({
+            error: false,
+            error_code: 200,
+            message: 'Malumot o\'chirildi',
+            data: model
+        });
+    }
     queueAll = async (req, res, next) => {
         const model = await QueueModel.findAll({
             where:{
